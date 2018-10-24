6914a431956486081c398bcd826486b8ef447f8e
Kevin Santos <kevinochieng548@gmail.com>
2018-10-24 21:59:25 +0300

    create an article with title and body

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -6,9 +6,24 @@ module Api
         render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
       end
 
+      def create
+        article = Article.new(article_params)
+        if article.save
+          render json: {status: 'SUCCESS',message: 'Saved Article', data: article}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, 
+            status: :unprocessable_entity
+        end
+      end
+
       def show
         article = Article.find(params[:id])
         render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
+      end
+
+      private
+      def article_params
+        params.permit(:title, :body)
       end
     end
   end
