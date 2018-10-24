e81b86a6ae5e56e53170c47d5389d13a0e0c92c0
Kevin Santos <kevinochieng548@gmail.com>
2018-10-24 22:41:09 +0300

    define update action and return updated article in json

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -21,6 +21,16 @@ module Api
         render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
       end
 
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'SUCCESS', message: 'Updated article', data: article}, status: :ok
+        else
+          render json: {status: 'ERROR', message: 'Article not updated', data: article.errors},
+            status: :unprocessable_entity
+        end
+      end
+
       def destroy
         article = Article.find(params[:id])
         article.destroy
